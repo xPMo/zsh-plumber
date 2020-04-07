f55c015bd2d7e21893842a59512b5ed8052a4374
gammafn <GammaFunction@vivaldi.net>
2020-04-07 12:37:07 -0500

    change arch

--- zplumber.zsh
+++ zplumber.zsh
@@ -1,5 +1,13 @@
 fpath+=("${0:h}/functions")
 autoload -Uz →plumber-accept-line →plumber-line-init add-zle-hook-widget
 
-zle -N accept-line →plumber-accept-line
-zle -N →plumber-line-init
+
+zle -N →plumber-accept-line
+add-zle-hook-widget line-finish →plumber-accept-line
+
+local m
+for m in viins vicmd emacs; do
+	# ALT+ENTER
+	bindkey '^[^M' →plumber-accept-line
+	bindkey '^[^J' →plumber-accept-line
+done
