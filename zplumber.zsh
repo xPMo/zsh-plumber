aeca660aac6fb9657129a7728434985cebaf9c31
gammafn <GammaFunction@vivaldi.net>
2020-04-11 12:32:04 -0500

    Simplify names, fix history

--- zplumber.zsh
+++ zplumber.zsh
@@ -1,12 +1,12 @@
 fpath+=("${0:h}/functions")
-autoload -Uz →plumber-line-finish →plumber-accept →plumber-line-init \
+autoload -Uz →plumber-line-finish →plumber-accept →plumber-line-init →plumber-relay \
 	add-zle-hook-widget split-shell-arguments
 zmodload zsh/zselect
 
 add-zle-hook-widget line-finish →plumber-line-finish
 zle -N →plumber-accept
 
-local m
+local m=
 for m (-finish -test '')
 	zle -N →plumber-line-init{$m,}
 
