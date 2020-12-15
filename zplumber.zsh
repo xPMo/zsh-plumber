dbf55eff9dcef3bb6b66ab822210e41dd5f3bcf0
gammafn <GammaFunction@vivaldi.net>
2020-12-14 22:52:53 -0600

    anon function for local m

--- zplumber.zsh
+++ zplumber.zsh
@@ -6,12 +6,14 @@ zmodload zsh/zselect
 add-zle-hook-widget line-finish →plumber-line-finish
 zle -N →plumber-accept
 
-local m=
-for m (-finish -test '')
-	zle -N →plumber-line-init{$m,}
+(){
+	local m
+	for m (-finish -test '')
+		zle -N →plumber-line-init{$m,}
 
-for m in viins vicmd visual emacs; do
-	# ALT+ENTER
-	bindkey -M $m '^[^M' →plumber-accept
-	bindkey -M $m '^[^J' →plumber-accept
-done
+	for m in viins vicmd visual emacs; do
+		# ALT+ENTER
+		bindkey -M $m '^[^M' →plumber-accept
+		bindkey -M $m '^[^J' →plumber-accept
+	done
+}
